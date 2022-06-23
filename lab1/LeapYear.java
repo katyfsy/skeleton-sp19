8eb019ee0b598e17a211ed6fdb7d5410ec33e9d6
Katy F <katyf@katys-mbp.lan>
2022-06-23 16:25:15 -0400

    lab1 completed

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -29,5 +29,20 @@ public class LeapYear {
             }
         }
     }
+    /**
+        A leap year is either:
+            divisible by 400 or
+            divisible by 4 and not by 100.
+        For example, 2000 and 2004 are leap years. 1900, 2003, and 2100 are not leap years.
+     */
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0){
+            return true;
+        } else if (year % 4 == 0 && year % 100 != 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 
