728c881cfeadf590ed6202826f5b0a82c3cf0a1e
katyfsy <katyfsy@gmail.com>
2022-06-24 22:56:59 -0400

    readBodies passed

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -5,4 +5,23 @@ public class NBody {
     return in.readDouble();
   }
 
+  public static Body[] readBodies(String fileName) {
+    In in = new In(fileName);
+    int count = in.readInt();
+    double radius = in.readDouble();
+    Body[] bodies = new Body[count];
+    int i = 0;
+    while (i < count) {
+      double xP = in.readDouble();
+      double yP = in.readDouble();
+      double xV = in.readDouble();
+      double yV = in.readDouble();
+      double m = in.readDouble();
+      String img = in.readString();
+      bodies[i] = new Body(xP, yP, xV, yV, m, img);
+      i += 1;
+    }
+    return bodies;
+  }
+
 }
